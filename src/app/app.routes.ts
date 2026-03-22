431f81008344a38f76dc232b74698fa20f542686
Ren <watrista@gmail.com>
2026-03-22 16:28:24 -0600

    Implementación de la ruta del catalogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './layouts/public-layout/public-layout';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    //Rutas públicas
+    {
+        path: '',
+        component: PublicLayout,
+        children: [
+        {
+            path: '',
+            loadComponent: () =>
+            import('./modules/catalogo/catalogo-publico/catalogo-publico')
+                .then(m => m.CatalogoPublico)
+        }
+        ]
+    },
+];
