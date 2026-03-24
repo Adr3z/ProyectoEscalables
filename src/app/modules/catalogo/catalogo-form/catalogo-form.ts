d83e203075cfced9f4bbf516afddda138a4d1c34
Ren <watrista@gmail.com>
2026-03-23 23:55:28 -0600

    Cambio al modal para crear producto
    Cambio a cómo se escogen las categorías y ahora se puede elegir el stock minimo y maximo desde la creación o edición de un producto

--- src/app/modules/catalogo/catalogo-form/catalogo-form.ts
+++ src/app/modules/catalogo/catalogo-form/catalogo-form.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, isDevMode, OnChanges, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Modal } from '../../../shared/components';
-import { Producto } from '../../../models';
+import { Producto, Categoria } from '../../../models';
 import { CATEGORIAS_PRINCIPALES, SUBCATEGORIAS_PASTELES } from '../../../shared/data/mock.data';
 
 @Component({
@@ -18,39 +18,54 @@ export class CatalogoForm implements OnChanges {
   @Input()  producto: Producto | null = null;
   @Output() cerrar   = new EventEmitter<void>();
 
-  readonly categoriasPrincipales = CATEGORIAS_PRINCIPALES;
-  readonly subcategoriasPasteles = SUBCATEGORIAS_PASTELES;
+  categoriasPrincipales: Categoria[] = CATEGORIAS_PRINCIPALES;
+  subcategoriasPasteles: Categoria[] = SUBCATEGORIAS_PASTELES;
 
-  form = { nombre: '', precio: 0, categoriaId: '', descripcion: '' };
-  padreSeleccionadoId = '';
+  form: {
+    nombre:       string;
+    precio:       number | null;
+    categoriaId:  string;
+    descripcion:  string;
+    stockMinimo:  number | null;
+    stockMaximo:  number | null;
+  } = {
+    nombre: '', precio: null, categoriaId: '', descripcion: '',
+    stockMinimo: null, stockMaximo: null
+  };
+
 
   get titulo(): string { return this.producto ? 'Editar producto' : 'Crear nuevo producto'; }
   get esEdicion(): boolean { return !!this.producto; }
-  get mostrarSubcategorias(): boolean { return this.padreSeleccionadoId === 'c1'; }
+
+  get mostrarSubcategorias(): boolean {
+    return this.form.categoriaId === 'c1' ||
+      this.subcategoriasPasteles.some(s => s._id === this.form.categoriaId);
+  }
 
   ngOnChanges(): void {
     if (this.producto) {
-      const esSub = this.subcategoriasPasteles.some(s => s._id === this.producto!.categoriaId);
-      this.padreSeleccionadoId = esSub ? 'c1' : this.producto.categoriaId;
       this.form = {
         nombre:      this.producto.nombre,
         precio:      this.producto.precio,
         categoriaId: this.producto.categoriaId,
-        descripcion: this.producto.descripcion ?? ''
+        descripcion: this.producto.descripcion ?? '',
+        stockMinimo: null,
+        stockMaximo: null,
       };
     } else {
-      this.padreSeleccionadoId = '';
-      this.form = { nombre: '', precio: 0, categoriaId: '', descripcion: '' };
+      this.form = { 
+        nombre: '', 
+        precio: null, 
+        categoriaId: '', 
+        descripcion: '',
+        stockMinimo: null,
+        stockMaximo: null,
+      };
     }
   }
 
-  onPadreChange(): void {
-    //Si cambia a algo que no es Pasteles, asignar directo
-    if (this.padreSeleccionadoId !== 'c1') {
-      this.form.categoriaId = this.padreSeleccionadoId;
-    } else {
-      this.form.categoriaId = '';
-    }
+  seleccionarCategoriaPrincipal(id: string): void{
+    this.form.categoriaId = id;
   }
 
   guardar(): void {
