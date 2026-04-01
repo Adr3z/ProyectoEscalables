bb306c90fd0dac482a829c9978de2124b3f73853
Ren <watrista@gmail.com>
2026-04-01 00:17:41 -0600

    Configuración inicial del servidor y conexión a mongo

---
+++ backend/src/config/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const conectarDB = require('./db');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Prefijos para las rutas
+        this.paths = {
+            categorias: '/api/categorias',
+            productos: '/api/productos',
+            inventario: '/api/inventario',
+            ventas: '/api/ventas',
+            usuarios: '/api/usuarios',
+        };
+
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    async conectarDB() {
+        await conectarDB();
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.json({ ok: true, mensaje: 'BakePOS API corriendo' });
+        });
+    }
+
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor levantado en el puerto ${this.port}`);
+        });
+    }
+}
+
+module.exports = Server;
